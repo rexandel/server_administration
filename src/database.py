ca846a5e385908fcf13502c18f562ec4603fb6d0
Alexander Sidorenko <xarendel@gmail.com>
2025-06-19 14:10:44 +0300

    fix: problems with tests

--- src/database.py
+++ src/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
-
+import os
 
 DATABASE_URL = os.getenv(
     "DATABASE_URL", 
